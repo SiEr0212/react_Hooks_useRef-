0ef6d3784c6678577268a158093b976fd84cb299
SiEr0212 <simonertel@gmx.de>
2021-04-30 22:29:26 +0200

    added refs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const marriedRef = useRef();
+  const submitRef = useRef();
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h3>UseRefs Hook</h3>
+        <div className="form-field">
+          <span>Name: </span>
+          <input ref={nameRef} type="text" />
+        </div>
+        <div className="form-field">
+          <span>age: </span>
+          <input ref={ageRef} type="text" />
+        </div>
+        <div className="form-field">
+          <span>Married? </span>
+          <input ref={marriedRef} type="checkbox" />
+        </div>
+        <button ref={submitRef}>Submit</button>
+      </header>
     </div>
   );
 }
